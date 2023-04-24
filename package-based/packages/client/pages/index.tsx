47fd7a5c303e8f7f7f449dcb0c519d79279b4e2f
tam.nguyen <tam.nguyen@zodinet.com>
2023-04-24 09:57:05 +0700

    build

--- package-based/packages/client/pages/index.tsx
+++ package-based/packages/client/pages/index.tsx
@@ -1,13 +1,10 @@
 import { TextField } from '@/../ui-components';
 import { Template } from '@/../ui-toolkit';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
     return (
         <main
-            className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+            className={`flex min-h-screen flex-col items-center justify-between p-24 `}
         >
             <TextField
                 label='Your name'
